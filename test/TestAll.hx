03d3cf852552dd96dcb60889d6da56815c64ba1e
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-08-28 14:27:42 -0600

    basic setup

---
+++ test/TestAll.hx
@@ -0,0 +1,19 @@
+import utest.UTest;
+
+class TestAll {
+  public function new() {}
+
+  public function testLoadFromFile() {
+
+  }
+
+  public function testLoadFromHttp() {
+
+  }
+
+  public static function main() {
+    UTest.run([
+      new TestAll()
+    ]);
+  }
+}
